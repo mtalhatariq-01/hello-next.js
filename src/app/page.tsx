315cbbe8dccd2da9fe4d6d3079007c875f9aee3a
MUHAMMAD TALHA TARIQ <151367252+Talha9417@users.noreply.github.com>
2024-03-21 02:46:14 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Hello() {
-    return<div>Hello Next.js</div>
+    return<div>Hello, I am Muhammad Talha Tariq and this is my first Next.js project with manual deployment.</div>
 }
